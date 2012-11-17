719e5ee9150610aa24c71d9daf0e710dbb4a73af
Abbas Mashayekh <martianboy2005@gmail.com>
2012-11-17 19:15:48 +0330

    completed feed forward step

--- mlp.py
+++ mlp.py
@@ -1,34 +1,55 @@
 import numpy as np
+from math import tanh
 
-class NNet:
+class MLP:
     def __init__(self, structure):
-        self.structure = structure
-        self.weights = []
+        self.structure = np.array(structure,np.uint8)
+        self.weightMatrices = []
+
+        # Node storage format: (excitation, derivative, delta)
+        self.nodeStorage = [np.ndarray((nou,3)) for nou in self.structure[1:]]
+        self.layersCount = len(structure)
+
+        #self.alpha = 
 
         np.random.seed()
 
-        for k in range(1,len(structure)):
-            self.weights.append(np.random.rand(self.structure[k-1],self.structure[k]) - 0.5
+        for k in range(1,self.layersCount):
+            self.weightMatrices.append(np.random.rand(self.structure[k-1],
+                                                    self.structure[k]) - 0.5)
 
+    # TODO: Take learning rate into account
     def train(self, dataset, epsilon = 0.00005, N = 50):
         while True:
             epochError = 0
             for pattern,desiredValues in dataset:
-                out = self.feedForward(pattern)
-                deltaOut = map(lambda o,d: o*(1-o) * (d-o), out, desiredValues)
-
-                epochError += sum(map(lambda o,d: (o-d)**2, out, desiredValues)) / 2
                 
-                deltaHidden = self.backpropagate(deltaOut)
+                # Feed the pattern forward in the network
+                self.feedForward(pattern)
+
+                out,outDeriv = self.nodeStorage[self.layersCount - 1]
+
+                # Add quadratic deviation for this pattern to the epoch error
+                epochError += (desiredValues - out)**2 / 2
+
+                # Backpropagate error to the output layer units
+                nodeStorage[self.layersCount - 2][:,3] =
+                                            (desiredValues - out) * outDeriv
+                # Backpropagate error to the hidden units
+                self.backpropagate(deltaOutputLayer)
+
+                # Update weights based on the computed deltas and learning rate
                 self.updateWeights(deltaHidden)
 
             if epochError < epsilon: break
 
     def feedForward(self, pattern):
-        X = pattern
-        for weightsOfLayer in self.weights
-            X = weightsOfLayer.dot(X)
-        return X
+        sigmoid = np.vectorize(tanh)
+        o = pattern
+        for layer,weightsOfLayer in enumerate(self.weightMatrices):
+            o = sigmoid(o.dot(weightsOfLayer))
+            op = o*(1-o)
+            self.nodeStorage[layer][:,1:2] = zip(o,op)
 
     def backpropagate(self, deltaOut):
         pass
