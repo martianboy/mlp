fa448f5211fcb70ae1cef54e7c417a6ec596db08
Abbas Mashayekh <martianboy2005@gmail.com>
2012-11-20 17:49:22 +0330

    implemented simple backpropagation and weight update

--- mlp.py
+++ mlp.py
@@ -2,20 +2,18 @@ import numpy as np
 from math import tanh
 
 class MLP:
-    def __init__(self, structure):
+    def __init__(self, structure, alpha = 0.1):
         self.structure = np.array(structure,np.uint8)
         self.weightMatrices = []
 
         # Node storage format: (excitation, derivative, delta)
         self.nodeStorage = [np.ndarray((nou,3)) for nou in self.structure[1:]]
-        self.layersCount = len(structure)
-
-        #self.alpha = 
+        self.layersCount = len(structure) - 1
 
         np.random.seed()
 
         for k in range(1,self.layersCount):
-            self.weightMatrices.append(np.random.rand(self.structure[k-1],
+            self.weightMatrices.append(np.random.rand(self.structure[k-1] + 1,
                                                     self.structure[k]) - 0.5)
 
     # TODO: Take learning rate into account
@@ -27,19 +25,20 @@ class MLP:
                 # Feed the pattern forward in the network
                 self.feedForward(pattern)
 
-                out,outDeriv = self.nodeStorage[self.layersCount - 1]
+                out,outDeriv = \
+                    np.dstack(self.nodeStorage[self.layersCount - 1][:,0:2])[0]
 
                 # Add quadratic deviation for this pattern to the epoch error
-                epochError += (desiredValues - out)**2 / 2
+                epochError += np.linalg.norm(desiredValues - out)**2 / 2
 
                 # Backpropagate error to the output layer units
-                nodeStorage[self.layersCount - 2][:,3] =
+                self.nodeStorage[self.layersCount - 1][:,2] = \
                                             (desiredValues - out) * outDeriv
                 # Backpropagate error to the hidden units
-                self.backpropagate(deltaOutputLayer)
+                self.backpropagate()
 
                 # Update weights based on the computed deltas and learning rate
-                self.updateWeights(deltaHidden)
+                self.updateWeights()
 
             if epochError < epsilon: break
 
@@ -47,11 +46,20 @@ class MLP:
         sigmoid = np.vectorize(tanh)
         o = pattern
         for layer,weightsOfLayer in enumerate(self.weightMatrices):
+            o = np.concatenate((o,[1]))
             o = sigmoid(o.dot(weightsOfLayer))
             op = o*(1-o)
-            self.nodeStorage[layer][:,1:2] = zip(o,op)
+            self.nodeStorage[layer][:,0:2] = zip(o,op)
 
-    def backpropagate(self, deltaOut):
-        pass
-    def updateWeights(self, deltaHidden):
-        pass
+    def backpropagate(self):
+        delta = self.nodeStorage[self.layersCount - 1][:,2]
+        for layer in range(self.layersCount - 2, -1, -1):
+            D = np.diag(self.nodeStorage[layer][:,2])
+            W = self.weightMatrices[layer]
+            self.nodeStorage[layer][:,2] = delta = D.dot(W).dot(delta)
+
+    def updateWeights(self):
+        for layer in range(self.layersCount - 1):
+            delta = np.vstack(self.nodeStorage[layer][:,2])
+            ohat = np.dstack(self.nodeStorage[layer][:,0])[0]
+            self.weightMatrices[layer] += -self.alpha * delta.dot(ohat)
