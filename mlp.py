625c19a19ab5aa90c877875e18a263fec5cf5786
Abbas Mashayekh <martianboy2005@gmail.com>
2012-11-25 01:15:28 +0330

    It\'s working.

--- mlp.py
+++ mlp.py
@@ -2,87 +2,90 @@ import numpy as np
 
 
 class MLP:
-    def __init__(self, structure, learning_rate=0.1, momentum_effect=0.1):
-        self.structure = np.array(structure, np.uint8)
-        self.layersCount = len(structure) - 1
+    def __init__(self, shape):
+        self.shape = np.array(shape)
+        self.layersCount = len(shape) - 1
 
-        self.learning_rate = learning_rate
-        self.momentum_effect = momentum_effect
+        self.eta = 0.1
+        self.alpha = 0.1
 
-        self.epoch = 0
-        self.pattern = []
+        self.sigmoid = lambda x: 1 / (1 + np.exp(-5 * x))
+        self.dsigmoid = lambda o: 5 * o * (1 - o)
 
-        # Node storage format: (excitation, derivative, delta)
-        self.storage = {'nodes': [], 'deltaWeights': []}
-        self.storage['nodes'] = [np.zeros((nou, 3)) for nou in self.structure[1:]]
+        self.nodeValues = [np.ones((1, n)) for n in shape]
+        self.deltas = [np.zeros((1, n)) for n in shape[1:]]
+        self.weights = []
+        self.deltaWeights = []
 
         np.random.seed()
 
-        self.weightMatrices = []
         for k in range(self.layersCount):
-            W = np.random.rand(structure[k] + 1, structure[k + 1])
-            self.weightMatrices.append((W - 0.5) / 4)
-            self.storage['deltaWeights'].append(np.zeros((structure[k] + 1, structure[k + 1])))
+            Wbar = (np.random.rand(shape[k] + 1, shape[k + 1]) - 0.5) / 2
+            self.weights.append(Wbar)
+            self.deltaWeights.append(np.zeros((shape[k] + 1, shape[k + 1])))
 
-    def train(self, dataset, epsilon=0.0005, N=2500):
-        while True:
-            self.epoch += 1
+    def train(self, dataset, epsilon=0.01, epochs=2500, eta=0.1, alpha=0.1):
+        self.eta = eta
+        self.alpha = alpha
+
+        epoch = 0
+        while True:         # Epochs
+            epoch += 1
             epochError = 0
-            for pattern, desiredValues in dataset:
 
-                # Feed the pattern forward in the network
-                self.feedForward(pattern)
+            np.random.shuffle(dataset)
 
-                out, outDeriv = np.dstack(self.storage['nodes'][-1][:, 0:2])[0]
-                e = desiredValues - out
+            for pattern, desired in dataset:
 
-                # Add quadratic deviation for this pattern to the epoch error
-                epochError += np.linalg.norm(e) ** 2 / 2
+                out = self.feedForward(pattern)
 
-                # Backpropagate error to the output layer units
-                self.storage['nodes'][-1][:, 2] = np.diag(outDeriv).dot(e)
+                # epochError += 0.5 * ||out - desired||**2
+                error = out - desired
+                epochError += 0.5 * np.linalg.norm(error) ** 2
 
-                # Backpropagate error to the hidden units
-                self.backpropagate()
+                self.backpropagate(error)
 
-                # Update weights based on the computed deltas and learning rate
                 self.updateWeights()
 
-            print(epochError)
-
-            if epochError < epsilon:
+            if epoch >= epochs:
+                print('Did not converge after ', epoch, ' epochs.')
                 break
-            if self.epoch >= N:
+            if epochError <= epsilon:
+                print('Converged at epoch ', epoch)
                 break
 
+    # returns the netowk output
+    # saves all mid-layer output values in self.nodeValues
     def feedForward(self, pattern):
-        sigmoid = np.tanh
-        self.pattern = pattern
+        o = self.nodeValues[0] = pattern
 
-        o = pattern
-        for layer, weightsOfLayer in enumerate(self.weightMatrices):
-            o = np.concatenate((o, [1]))
-            o = sigmoid(o.dot(weightsOfLayer))
-            op = 1 - o ** 2
-            self.storage['nodes'][layer][:, 0:2] = list(zip(o, op))
+        for k in range(self.layersCount):
+            W = self.weights[k]
+            ohat = np.concatenate((o, (1,)))
 
-    def backpropagate(self):
-        delta = self.storage['nodes'][-1][:, 2]
-        for layer in range(self.layersCount - 2, -1, -1):
-            D = np.diag(self.storage['nodes'][layer][:, 1])
-            W = self.weightMatrices[layer + 1][:-1]
-            self.storage['nodes'][layer][:, 2] = delta = D.dot(W).dot(delta)
+            o = self.sigmoid(ohat.dot(W))
+            self.nodeValues[k + 1] = o
+
+        return o
+
+    def backpropagate(self, error):
+        e = error
+        for k in range(self.layersCount - 1, -1, -1):
+            o = self.nodeValues[k + 1]
+            D2 = np.diag(self.dsigmoid(o))
+
+            self.deltas[k] = delta = D2.dot(e)
+
+            W2 = self.weights[k][:-1]
+            e = W2.dot(delta)
 
     def updateWeights(self):
-        ohat = np.vstack(self.pattern + (1,))
+        for k in range(self.layersCount):
+            ohat = np.vstack(np.concatenate((self.nodeValues[k], (1,))))
+            delta = np.atleast_2d(self.deltas[k])
 
-        for layer in range(self.layersCount):
-            delta = np.atleast_2d(self.storage['nodes'][layer][:, 2])
-            self.storage['deltaWeights'][layer][:, :] = \
-                self.momentum_effect * self.storage['deltaWeights'][layer] - \
-                self.learning_rate * ohat.dot(delta)
+            self.deltaWeights[k] = \
+                -self.eta * ohat.dot(delta)  \
+                + self.alpha * self.deltaWeights[k]
 
-            #print(self.weightMatrices[layer])
-
-            self.weightMatrices[layer] += self.storage['deltaWeights'][layer]
-            ohat = np.vstack(np.concatenate((self.storage['nodes'][layer][:, 0], (1,))))
+            self.weights[k] += self.deltaWeights[k]
--- xor.py
+++ xor.py
@@ -1,10 +1,21 @@
-import mlp, numpy as np
+#import numpy as np
+import mlp
+
 
 def main():
-    dataset = [((0,0),0),((0,1),1),((1,0),1),((1,1),0)]
-    
+    dataset = [((0, 0), 0), ((0, 1), 1), ((1, 0), 1), ((1, 1), 0)]
+
     m = mlp.MLP([2, 2, 1])
-    m.train(dataset)
+    m.train(dataset, eta=0.6, epochs=1000)
+
+    print(m.feedForward((0, 0)))
+    print(m.feedForward((0, 1)))
+    print(m.feedForward((1, 0)))
+    print(m.feedForward((1, 1)))
+
+    for i in range(len(dataset)):
+        o = m.feedForward(dataset[i][0])
+        print(i, dataset[i][0], '%.2f' % o[0], ' (expected %.2f)' % dataset[i][1])
 
 if __name__ == '__main__':
     main()
